2e8f4f26ab47cdeb43b55f85c35660b50f435837
sb06ng <sb06ng01@insightxc.com>
2025-12-29 12:01:54 +0200

    Added scientific package

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
 from .basic import addition, subtraction, multiplication, division
+from .scientific import sqrt, power, sine, log, factorial, tangent
 
-__all__ = ["addition", "subtraction", "multiplication", "division"]
+__all__ = ["addition",
+           "subtraction",
+           "multiplication",
+           "division",
+           "sqrt",
+           "power",
+           "sine",
+           "log",
+           "factorial",
+           "tangent",
+           ]
